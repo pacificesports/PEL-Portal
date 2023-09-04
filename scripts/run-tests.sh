7804b6f2361f51aba07fab8f663b775b4f032bc5
Bharat Kathi <bharat1031@gmail.com>
2023-09-03 20:15:13 -0700

    add run tests script

---
+++ scripts/run-tests.sh
@@ -0,0 +1,6 @@
+dart pub global activate full_coverage
+dart pub global run full_coverage
+flutter analyze
+flutter test --coverage
+genhtml coverage/lcov.info -o coverage/html
+open coverage/html/index.html
