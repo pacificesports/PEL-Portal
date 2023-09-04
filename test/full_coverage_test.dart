a07cde5bb55dd286d7b0a722bdefa882ef6977d2
Bharat Kathi <bharat1031@gmail.com>
2023-09-03 19:57:12 -0700

    update test action

---
+++ test/full_coverage_test.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: unused_import
+import 'package:pel_portal/firebase_options.dart';
+import 'package:pel_portal/main.dart';
+import 'package:pel_portal/models/connection.dart';
+import 'package:pel_portal/models/privacy.dart';
+import 'package:pel_portal/models/school.dart';
+import 'package:pel_portal/models/user.dart';
+import 'package:pel_portal/models/user_school.dart';
+import 'package:pel_portal/models/verification.dart';
+import 'package:pel_portal/models/version.dart';
+import 'package:pel_portal/pages/auth/auth_checker.dart';
+import 'package:pel_portal/pages/auth/login_page.dart';
+import 'package:pel_portal/pages/auth/register_page.dart';
+import 'package:pel_portal/pages/not_found_page.dart';
+import 'package:pel_portal/pages/onboarding/onboarding_page.dart';
+import 'package:pel_portal/utils/alert_service.dart';
+import 'package:pel_portal/utils/auth_service.dart';
+import 'package:pel_portal/utils/config.dart';
+import 'package:pel_portal/utils/layout.dart';
+import 'package:pel_portal/utils/logger.dart';
+import 'package:pel_portal/utils/theme.dart';
+import 'package:pel_portal/widgets/buttons/pel_text_button.dart';
+import 'package:pel_portal/widgets/headers/public_header.dart';
+
+void main() {}
