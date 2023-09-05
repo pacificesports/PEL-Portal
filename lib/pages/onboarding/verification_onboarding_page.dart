f1c2fc7757fa8b4670be1db34221c6efb8e14948
Bharat Kathi <bharat1031@gmail.com>
2023-09-05 07:02:19 -0700

    email verification on onboarding page

--- lib/pages/onboarding/verification_onboarding_page.dart
+++ lib/pages/onboarding/verification_onboarding_page.dart
@@ -1,5 +1,10 @@
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart' as fb;
 import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
+import 'package:pel_portal/utils/alert_service.dart';
+import 'package:pel_portal/utils/auth_service.dart';
 import 'package:pel_portal/utils/config.dart';
 import 'package:pel_portal/utils/layout.dart';
 import 'package:pel_portal/utils/theme.dart';
@@ -14,6 +19,59 @@ class VerificationOnboardingPage extends StatefulWidget {
 }
 
 class _VerificationOnboardingPageState extends State<VerificationOnboardingPage> {
+
+  bool emailVerificationLoading = false;
+  Timer? emailVerificationTimer;
+
+  @override
+  void setState(fn) {
+    if (mounted) {
+      super.setState(fn);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    if (AuthService.verifyUserSession(context, "/onboarding/verification")) {
+      checkEmailVerification();
+    }
+  }
+
+
+  @override
+  void dispose() {
+    super.dispose();
+    emailVerificationTimer?.cancel();
+  }
+
+  void sendEmailVerification() {
+    fb.FirebaseAuth.instance.currentUser!.sendEmailVerification().then((value) {
+      AlertService.showSuccessSnackbar(context, "Verification email sent!");
+      setState(() => emailVerificationLoading = true);
+      emailVerificationTimer = Timer.periodic(const Duration(seconds: 5), (timer) {
+        checkEmailVerification();
+      });
+    }).catchError((err) {
+      AlertService.showErrorSnackbar(context, "Failed to send verification email!");
+    });
+  }
+
+  void checkEmailVerification() {
+    fb.FirebaseAuth.instance.currentUser!.reload().then((value) {
+      if (fb.FirebaseAuth.instance.currentUser!.emailVerified) {
+        AlertService.showSuccessSnackbar(context, "Email verified!");
+        setState(() {
+          currentUser.verification.isEmailVerified = true;
+          emailVerificationLoading = false;
+        });
+        emailVerificationTimer?.cancel();
+      }
+    }).catchError((err) {
+      AlertService.showErrorSnackbar(context, "Failed to check email verification!");
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -64,6 +122,27 @@ class _VerificationOnboardingPageState extends State<VerificationOnboardingPage>
                                   child: Column(
                                     children: [
                                       const Padding(padding: EdgeInsets.all(8)),
+                                      Card(
+                                        color: Theme.of(context).colorScheme.background,
+                                        child: ListTile(
+                                          contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                                          title: Text("Email Verification (${currentUser.email})"),
+                                          trailing: SizedBox(
+                                            height: 50,
+                                            child: !currentUser.verification.isEmailVerified && !emailVerificationLoading ? PELTextButton(
+                                              text: "Verify",
+                                              style: PELTextButtonStyle.filled,
+                                              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 0),
+                                              onPressed: () {
+                                                sendEmailVerification();
+                                              },
+                                            ) : emailVerificationLoading ? const RefreshProgressIndicator(
+                                                backgroundColor: PEL_MAIN,
+                                                color: Colors.white,
+                                            ) : const Icon(Icons.check_circle_rounded, color: PEL_SUCCESS),
+                                          )
+                                        ),
+                                      ),
                                       const Text(
                                         "Upload a photo of your student ID card or other proof of enrollment.",
                                       ),
