7598f42ad2ddcf2c2e090f2dcf2ea63587a3233b
Bharat Kathi <bharat1031@gmail.com>
2023-09-09 01:06:30 -0700

    portal header dropdown button

--- lib/widgets/headers/portal_header.dart
+++ lib/widgets/headers/portal_header.dart
@@ -23,46 +23,121 @@ class _PortalHeaderState extends State<PortalHeader> {
         Container(
           color: Theme.of(context).cardColor,
           width: LayoutHelper.width(context),
+          padding: const EdgeInsets.symmetric(horizontal: 32),
           height: 85,
-          child: Center(
-            child: Container(
-              color: Theme.of(context).cardColor,
-              width: LayoutHelper.getContentWidth(context),
-              padding: EdgeInsets.symmetric(horizontal: LayoutHelper.getPaddingDouble(context)),
-              child: Stack(
-                fit: StackFit.expand,
-                alignment: Alignment.center,
+          child: Stack(
+            fit: StackFit.expand,
+            alignment: Alignment.center,
+            children: [
+              SvgPicture.asset(
+                "assets/images/pel_abbrev/abbrev-mono.svg",
+                height: 75,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  SvgPicture.asset(
-                    "assets/images/pel_abbrev/abbrev-mono.svg",
-                    height: 75,
+                  Text(
+                    "Welcome, ${currentUser.firstName}!",
+                    style: const TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
                   ),
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisAlignment: MainAxisAlignment.end,
                     children: [
-                      Text(
-                        "Welcome, ${currentUser.firstName}!",
-                        style: const TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(100),
-                            child: ExtendedImage.network(
-                              currentUser.profilePictureURL,
-                              height: 50,
-                              width: 50,
-                              fit: BoxFit.cover,
+                      PopupMenuButton(
+                        tooltip: "",
+                        offset: const Offset(0, 60),
+                        itemBuilder: (BuildContext context) {
+                          return [
+                            PopupMenuItem(
+                              onTap: () {
+                                Future.delayed(Duration.zero, () => router.navigateTo(context, "/profile", transition: TransitionType.fadeIn));
+                              },
+                              child: SizedBox(
+                                child: Column(
+                                  children: [
+                                    Row(
+                                      children: [
+                                        SizedBox(
+                                          width: 35,
+                                          child: ClipRRect(
+                                            borderRadius: BorderRadius.circular(100),
+                                            child: ExtendedImage.network(
+                                              currentUser.profilePictureURL,
+                                              height: 35,
+                                              width: 35,
+                                              fit: BoxFit.cover,
+                                            ),
+                                          ),
+                                        ),
+                                        const Padding(padding: EdgeInsets.only(right: 16)),
+                                        Column(
+                                          mainAxisAlignment: MainAxisAlignment.center,
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          children: [
+                                            Text("${currentUser.firstName} ${currentUser.lastName}", style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                                            Text("@${currentUser.getConnection("discord_username").connection}", style: const TextStyle(fontSize: 16, color: Colors.grey)),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
                             ),
-                          )
-                        ],
+                            PopupMenuItem(
+                              onTap: () {
+                                Future.delayed(Duration.zero, () => router.navigateTo(context, "/settings", transition: TransitionType.fadeIn));
+                              },
+                              child: const SizedBox(
+                                width: 200,
+                                child: Row(
+                                  children: [
+                                    SizedBox(
+                                      width: 35,
+                                      child: Icon(Icons.settings_outlined, color: PEL_MAIN)
+                                    ),
+                                    Padding(padding: EdgeInsets.only(right: 16)),
+                                    Text("Account Settings", style: TextStyle(fontSize: 16)),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            PopupMenuItem(
+                              onTap: () async {
+                                await AuthService.signOut();
+                                Future.delayed(Duration.zero, () => router.navigateTo(context, "/auth/check", clearStack: true, replace: true, transition: TransitionType.fadeIn));
+                              },
+                              child: const SizedBox(
+                                width: 200,
+                                child: Row(
+                                  children: [
+                                    SizedBox(
+                                        width: 35,
+                                        child: Icon(Icons.logout_rounded, color: PEL_MAIN)
+                                    ),
+                                    Padding(padding: EdgeInsets.only(right: 16)),
+                                    Text("Sign out", style: TextStyle(fontSize: 16)),
+                                  ],
+                                ),
+                              ),
+                            )
+                          ];
+                        },
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(100),
+                          child: ExtendedImage.network(
+                            currentUser.profilePictureURL,
+                            height: 50,
+                            width: 50,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
                       )
                     ],
-                  ),
+                  )
                 ],
               ),
-            ),
+            ],
           ),
         ),
         const Divider(color: PEL_MAIN, height: 0,)
