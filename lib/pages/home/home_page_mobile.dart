fe9a53046e5d81dd9f42afaf9b665a4d3119bdda
Bharat Kathi <bharat1031@gmail.com>
2023-09-09 12:41:29 -0700

    add mobile tabbar

---
+++ lib/pages/home/home_page_mobile.dart
@@ -0,0 +1,34 @@
+import 'package:dot_navigation_bar/dot_navigation_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:pel_portal/utils/auth_service.dart';
+
+class HomePageMobile extends StatefulWidget {
+  const HomePageMobile({super.key});
+
+  @override
+  State<HomePageMobile> createState() => _HomePageMobileState();
+}
+
+class _HomePageMobileState extends State<HomePageMobile> {
+
+  @override
+  void setState(fn) {
+    if (mounted) {
+      super.setState(fn);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("Home"),
+      ),
+    );
+  }
+}
