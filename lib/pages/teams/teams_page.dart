090148f314757927b687914a00deec7f014bdd67
Bharat Kathi <bharat1031@gmail.com>
2023-09-09 17:13:18 -0700

    created teams pages

---
+++ lib/pages/teams/teams_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TeamsPage extends StatefulWidget {
+  const TeamsPage({super.key});
+
+  @override
+  State<TeamsPage> createState() => _TeamsPageState();
+}
+
+class _TeamsPageState extends State<TeamsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/pages/teams/teams_page_mobile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TeamsPageMobile extends StatefulWidget {
+  const TeamsPageMobile({super.key});
+
+  @override
+  State<TeamsPageMobile> createState() => _TeamsPageMobileState();
+}
+
+class _TeamsPageMobileState extends State<TeamsPageMobile> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
